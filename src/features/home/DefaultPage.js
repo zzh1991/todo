d979029d069b342a07a8d309301d398c96c0ac11
zzh1991 <zzh19910530@163.com>
2018-05-02 23:35:01 +0800

    collapse menu

--- src/features/home/DefaultPage.js
+++ src/features/home/DefaultPage.js
@@ -13,8 +13,16 @@ export class DefaultPage extends Component {
     super(props);
     this.state = {
       toolbarTitle: 'Todo',
+      collapsed: false,
     };
   }
+
+  toggle = () => {
+    const { collapsed } = this.state;
+    this.setState({
+      collapsed: !collapsed,
+    });
+  };
 
   capitalizeFirstLetter = (text) => {
     return text.charAt(0).toUpperCase() + text.slice(1);
@@ -33,29 +41,34 @@ export class DefaultPage extends Component {
         <Header className="header" style={{ paddingLeft: 20 }} >
           <div className="logo" />
           <h1 style={{ color: 'white', fontSize: 20 }}>
-            <Icon type="schedule" />
+            <Icon type="schedule" onClick={this.toggle} />
             {' To Do List'}
           </h1>
         </Header>
         <Layout style={{ height: '92vh' }} >
-          <Sider width={200} style={{ background: '#fff' }}>
+          <Sider
+            trigger={null}
+            collapsible
+            collapsed={this.state.collapsed}
+          >
             <Menu
               mode="inline"
+              theme="dark"
               defaultSelectedKeys={['todo']}
               style={{ height: '100%', borderRight: 0 }}
               onClick={this.handleMenuChange}
             >
               <Menu.Item key="todo">
                 <Icon type="calendar" />
-                {'To Do'}
+                <span>To Do</span>
               </Menu.Item>
               <Menu.Item key="completed">
                 <Icon type="check-circle" />
-                {'Completed'}
+                <span>Completed</span>
               </Menu.Item>
               <Menu.Item key="deleted">
                 <Icon type="delete" />
-                {'Deleted'}
+                <span>Deleted</span>
               </Menu.Item>
             </Menu>
           </Sider>
